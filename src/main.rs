2b6f2fd0dcc9a6f66364f4cd89e5720b50deaa12
Laura Powles <laura@laurapowles.com>
2021-09-10 20:54:33 +1000

    cleaned up clap settings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 mod statusblock;
 
-use clap::{load_yaml, App, AppSettings};
+use clap::{load_yaml, App};
 use statusblock::StatusBlock;
 
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let _matches = App::from_yaml(yaml)
-        .set_term_width(0)
-        .settings(&[
-            AppSettings::DisableHelpSubcommand,
-            AppSettings::DeriveDisplayOrder,
-            // AppSettings::SubcommandRequiredElseHelp,
-        ])
-        .get_matches();
+    let cli_settings = load_yaml!("cli.yml");
+    let cli_matches = App::from_yaml(cli_settings).get_matches();
 
     let test_block =
         StatusBlock::new("test", || "this is a test block".to_string());
